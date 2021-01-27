bd9887385a28344249ca0efdafdbb9527efa7a82
YMGYM <good_day1234@naver.com>
2021-01-27 21:48:44 +0900

    20200127

---
+++ thisIsCodingTest/timeCompare.py
@@ -0,0 +1,34 @@
+from random import randint
+import time
+
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100)) # 랜덤한 정수
+
+# 선택 정렬 프로그램 성능 측정
+start_time = time.time()
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # 스와프
+
+end_time = time.time()
+print("선택 정렬 성능 측정:", end_time - start_time)
+
+# 배열을 다시 무작위 데이터로 초기화함
+
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
+
+# 기본 정렬 라이브러리 사용
+start_time = time.time()
+
+array.sort()
+
+end_time = time.time()
+
+print("기본 정렬 라이브러리 성능 ", end_time - start_time)
---
+++ thisIsCodingTest/timeMeasure.py
@@ -0,0 +1,9 @@
+import time
+
+start_time = time.time()
+
+# 프로그램 소스코드
+
+end_time = time.time()
+
+print("실행 시간 : ", start_time - end_time)
